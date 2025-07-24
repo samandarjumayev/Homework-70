d218a2f202050cb1e68ca35553e103dae0592ad8
samandarjumayev <samandarjumayev005@gmail.com>
2025-07-25 01:11:23 +0500

    Loading add

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 export default function Footer(){
-    return <div className="bg-[#109BE9] h-[50px] flex justify-center items-center text-white text-lg font-semibold">
+    return <div className="bg-[#109BE9] h-[50px] flex justify-center items-center text-white text-lg font-semibold cursor-pointer transition-all duration-500 hover:bg-gray-800 hover:text-[cyan]">
         <p>Created by Samandar | Since 2022</p>
     </div>
 }
